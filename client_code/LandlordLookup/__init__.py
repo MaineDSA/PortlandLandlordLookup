12b007b34b0354daf4c5297ddcd6ed5951226420
steering@mainedsa.org <steering@mainedsa.org>
2023-09-19 11:46:53 +0000

    Created Lookup; Edited settings, LandlordLookup, theme/parameters.yaml and LandlordLookup.UnitListings

--- client_code/LandlordLookup/__init__.py
+++ client_code/LandlordLookup/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import LandlordLookupTemplate
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -10,23 +11,15 @@ class LandlordLookup(LandlordLookupTemplate):
     self.init_components(**properties)
 
   def query_lookup(self):
-    # need at least an address
-    if self.textbox_address.text != "":
-      building_units = app_tables.units.search(
-          Address=q.ilike(f'%{self.textbox_address.text}%')
-        )
-      print("Found " + str(len(building_units)) + " building matches.")
-      if len(building_units) > 0:
-        unit_matches = app_tables.units.search(q.all_of(
-            Address=q.ilike(f'%{self.textbox_address.text}%'),
-            unitNumber1=q.ilike(f'%{self.textbox_address_unit.text}%')
-          ))
-        print("Found " + str(len(unit_matches)) + " unit matches.")
-        if len(unit_matches) > 0:
-          self.units.items = unit_matches
-        else:
-          self.units.items = building_units
+    # Give up if no address is supplied
+    if not self.textbox_address.text:
+      Notification("No street address entered.")
+      return False
 
+    self.units.items = anvil.server.call('find_units', address=self.textbox_address.text, unit=self.textbox_address_unit.text)
+    Notification('Found ' + str(len(self.units.items)) + 'matching units.')
+  
+  # Triggering when button is clicked
   def submit_click(self, **event_args):
     self.query_lookup()
 
---
+++ server_code/Lookup.py
@@ -0,0 +1,37 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+
+@anvil.server.callable
+def find_units(**kwargs):
+  address = kwargs.get('address', None)
+  unit = kwargs.get('unit', None)
+  
+  # Find all units that match the address
+  building_units = app_tables.units.search(
+      Address=q.ilike(f'%{address}%')
+    )
+  print("Found " + str(len(building_units)) + " building matches.")
+  
+  # Give up if no units found at address 
+  if len(building_units) == 0:
+    return False
+  
+  # Find all units that match the unit AND the address
+  unit_matches = app_tables.units.search(q.all_of(
+      Address=q.ilike(f'%{address}%'),
+      unitNumber1=q.ilike(f'%{unit}%')
+    ))
+  print("Found " + str(len(unit_matches)) + " unit matches.")
+  
+  # If there are specific unit matches, show them
+  # If not, show the building matches
+  if len(unit_matches) > 0:
+    return unit_matches
+  else:
+    return building_units
