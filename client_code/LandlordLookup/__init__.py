91dde3ddcf225f093e55e9b573772be280437bc1
steering@mainedsa.org <steering@mainedsa.org>
2023-09-20 15:52:33 +0000

    Edited LandlordLookup

--- client_code/LandlordLookup/__init__.py
+++ client_code/LandlordLookup/__init__.py
@@ -12,19 +12,15 @@ class LandlordLookup(LandlordLookupTemplate):
 
   def query_lookup(self):
     # Give up if no address is supplied
-    if not self.textbox_address.text:
-      Notification("No street address entered.")
+    if not self.textbox_landlord.text:
+      Notification("No landlord name entered.")
       return False
 
-    found_units = anvil.server.call('find_units', address=self.textbox_address.text, unit=self.textbox_address_unit.text)
+    found_units = anvil.server.call('find_units', landlord=self.textbox_landlord.text)
     if not found_units:
-      self.tenantcontact.visible = False
       return False
     self.units.items = found_units
     Notification('Found ' + str(len(self.units.items)) + 'matching units.')
-
-    if len(self.units.items) > 0:
-      self.tenantcontact.visible = True
 
   # Triggering when button is clicked
   def search_click(self, **event_args):
