cf10e0d8794600ea5fd04bc9a25c25d62fea9baf
steering@mainedsa.org <steering@mainedsa.org>
2023-09-20 20:06:05 +0000

    Edited LandlordLookup.LandlordBuildings

--- client_code/LandlordLookup/LandlordBuildings/__init__.py
+++ client_code/LandlordLookup/LandlordBuildings/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from anvil.js.window import navigator
 
 class LandlordBuildings(LandlordBuildingsTemplate):
   def __init__(self, **properties):
@@ -12,3 +13,8 @@ class LandlordBuildings(LandlordBuildingsTemplate):
 
     self.address.text = 'test'
     self.numberofrentalunits.text = 0
+
+  def copylink_click(self, **event_args):
+    navigator.clipboard.writeText("Hello World!")
+
+
