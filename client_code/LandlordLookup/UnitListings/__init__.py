f5d132a59b34b4fd31d3a6d66ad310d3662e22f9
steering@mainedsa.org <steering@mainedsa.org>
2023-09-20 00:30:13 +0000

    Edited LandlordLookup.UnitListings

--- client_code/LandlordLookup/UnitListings/__init__.py
+++ client_code/LandlordLookup/UnitListings/__init__.py
@@ -14,7 +14,7 @@ class UnitListings(UnitListingsTemplate):
     self.unitaddress.text = self.item['Address']
     self.unitbedrooms.text = self.item['nbrBedRms1']
     self.unitcurrentrent.text = self.item['CurrentRent1']
-    if self.item['Likely to Exempt']:
+    if self.item['Likely to Exempt'] == True:
       self.unitislikely.text = 'is likely to'
     else:
       self.unitislikely.text = 'may not'
