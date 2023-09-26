414bbf05e0cf606fe8bc2af75e46a3caab978745
steering@mainedsa.org <steering@mainedsa.org>
2023-09-26 20:18:48 +0000

    Created SaveData; Edited SubmitMessage and UnitLookup

--- server_code/SaveData/__init__.py
+++ server_code/SaveData/__init__.py
@@ -4,10 +4,9 @@ from anvil.tables import app_tables
 import anvil.server
 
 @anvil.server.callable
-def save_message(**kwargs):
+def save_data(**kwargs):
   return app_tables.messages.add_row(
-      firstname = kwargs.get('firstname', None),
-      lastname = kwargs.get('lastname', None),
       email = kwargs.get('email', None),
-      story = kwargs.get('story', None),
+      address = kwargs.get('address', None),
+      unit = kwargs.get('unit', None)
     )
